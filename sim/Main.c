e2bd8a25d444f4d0647e98c813bf1d6672828578
ItaiShchorry <itaishch@gmail.com>
2017-12-30 14:56:52 +0200

    main + issue schema
    main + issue schema

--- sim/Main.c
+++ sim/Main.c
@@ -2,13 +2,32 @@
 
 config_args* config_args_read;
 inst** instructions;
+inst* iq[16]; //our instruction queue
+int last_unoccupied_index_in_iq = 0;
 int memory_image_input[MEMORY_IMAGE_INPUT_SIZE];
 CDB** cdb;
+unsigned int cycles = 0;
+int current_inst_in_iq = 0;
+int current_inst_in_instructions = 0;
+
+void fetch_instruction()
+{
+	if (last_unoccupied_index_in_iq < 15)
+	{
+		iq[last_unoccupied_index_in_iq] = instructions[current_inst_in_instructions];
+		last_unoccupied_index_in_iq++;
+		current_inst_in_instructions++;
+	}
+}
+
+void fetch()
+{
+	fetch_instruction();
+	fetch_instruction();
+}
 
 int main(int argc, char* argv[])
 {
-
-
 
 	int return_value = SUCCESS;
 	int ret_args = parse_args(argv);
@@ -16,9 +35,115 @@ int main(int argc, char* argv[])
 	{
 		return FAIL;
 	}
-	
+	//while (instr_proc < instr_count) {
+	while (current_inst_in_instructions <= total_num_of_inst) {
+
+		cycles++;
+		fetch();
+
+		issue();
+
+		execute();
+
+		//write_back();
+		broadcast();
+
+	}
 
 	return return_value;
+}
+
+inst* take_next_instruction_from_IQ()
+{
+	/*will return an RS from the correct kind for specified instruction
+	return:
+	null - meaning there's no available RS in this cycle, meaning we won't issue anything in the cycle
+	otherwise - available RS*/
+	if(num_of_inst)
+}
+
+RS* get_free_reservation_station(inst* inst)
+{
+	/*will return an RS from the correct kind for specified instruction
+	return:
+	null - meaning there's no available RS in this cycle, meaning we won't issue anything in the cycle
+	otherwise - available RS*/
+}
+
+inst** determine_where_inputs_come_from(inst inst)
+{
+	/*will determine wether the inputs for the current instruction are already ready, or are they gonna be produced by some of the instructions that haven't yet broadcasted their results
+	return value:
+	empty array - means that the inputs are ready
+	otherwise - all instructions we must wait for them to be completed*/
+	inst** res = malloc(sizeof(inst)*MAX_INST_NUM);
+
+	return res;
+}
+void issue()
+{
+	
+	inst* inst = take_next_instruction_from_IQ();
+
+	inst** inst_to_wait_for = determine_where_inputs_come_from(inst);
+
+	RS* rs = get_free_reservation_station();
+
+	put_inst_in_RS();
+
+	tag_dest_register_of_inst(inst);
+	//int i, rs_count;
+	//Instruction *curr;
+	//RS *rs_type, *rs;
+	//curr = &iq[instr_proc];
+
+	///* Based on the opcode type, assign a common looping pointer and a counter {{{ */
+	//if (!(strcmp("ADD", curr->opcd)) || !(strcmp("SUB", curr->opcd))) {
+	//	rs_count = NUM_INT_ADD_RS; rs_type = add_int_rs;
+	//}
+	//else if (!(strcmp("ADDD", curr->opcd)) || !(strcmp("SUBD", curr->opcd))) {
+	//	rs_count = NUM_FLT_ADD_RS; rs_type = mul_int_rs;
+	//}
+	//else if (!(strcmp("MUL", curr->opcd)) || !(strcmp("DIV", curr->opcd))) {
+	//	rs_count = NUM_INT_MUL_RS; rs_type = add_fp_rs;
+	//}
+	//else if (!(strcmp("MULD", curr->opcd)) || !(strcmp("DIVD", curr->opcd))) {
+	//	rs_count = NUM_FLT_MUL_RS; rs_type = mul_fp_rs;
+	//}
+	//else if (!(strcmp("LD", curr->opcd))) {
+	//	rs_count = NUM_LD_RS; rs_type = ld_rs;
+	//}
+	//else if (!(strcmp("SD", curr->opcd))) {
+	//	rs_count = NUM_SD_RS; rs_type = sd_rs;
+	//}
+	///* }}} */
+
+	///* Grab the first available RS of the type selected in the previous step {{{ */
+	//for (i = 0; i < rs_count; i++)
+	//	if (rs_type[i].status == AVAILABLE)
+	//		break;
+
+	///* If all the RS are occupied, simply return back without incrementing */
+	///* the number of instructions executed. this causes the subsequent issue */
+	///* call to process the same instruction for issuing */
+
+	//if (i >= rs_count) return;
+
+	///* }}} */
+
+	///* now, the instruction is ready to be issued. we can get started */
+	//rs = &rs_type[i];
+	//rs->status = BUSY;
+	//rs->instr = curr;
+
+	//if (curr->src1)
+	//	rs->qj = dep_lookup(curr->src1);
+
+	//if (curr->src2)
+	//	rs->qk = dep_lookup(curr->src2);
+
+	//curr->issue_time = cycles;
+	//instr_proc++;
 }
 
 int parse_args(char* argv[])
